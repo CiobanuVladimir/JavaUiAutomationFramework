abefae4979f08b0ae25d24686eea861deb83e819
CiobanuVladimir <ciobanu.vladimir1@gmail.com>
2023-06-18 15:50:33 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,27 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DriverManager manager = DriverManager.getInstance();
+        WebDriver driver = manager.getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver");
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        manager.getDriver().get("https://stackoverflow.com/questions/17973970/how-can-i-solve-java-lang-noclassdeffounderror");
+        driver.quit();
+
+        System.out.println("The driver is closed");
+
+
     }
 }
